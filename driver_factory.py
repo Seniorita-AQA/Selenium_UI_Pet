274c921c2ec7a16a93ba4395a1de3e101033186f
Seniorita_AQA <nuta704@gmail.com>
2026-01-16 00:25:39 +0100

    Code refactoring

--- driver_factory.py
+++ driver_factory.py
@@ -1,19 +1,3 @@
-# from selenium import webdriver
-#
-#
-# def create_driver(browser):
-#     if browser == "chrome":
-#         return webdriver.Chrome()
-#     elif browser == "firefox":
-#         return webdriver.Firefox()
-#     elif browser == "edge":
-#         return webdriver.Edge()
-#     elif browser == "safari":
-#         return webdriver.Safari()
-#     else:
-#         raise ValueError(f"Unsupported browser: {browser}")
-
-
 from selenium import webdriver
 
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -21,7 +5,7 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 from selenium.webdriver.edge.service import Service as EdgeService
 
 
-# --- Paths to local driver binaries ---
+# Paths to local driver binaries
 CHROME_DRIVER_PATH = r"C:\Drivers\chromdriver\chromedriver.exe"
 FIREFOX_DRIVER_PATH = r"C:\Drivers\geckodriver-v0.36.0-win64\geckodriver.exe"
 EDGE_DRIVER_PATH = r"C:\Drivers\edgedriver_win64\msedgedriver.exe"
